9d7d90d1f722d70e0298a5c3479923658b5943ab
Steph Baker <42748054+stephbaker@users.noreply.github.com>
2019-02-03 11:56:07 -0500

    Create word_splitters.rb

---
+++ 6.2 Comparable and Enumerable/word_splitters.rb
@@ -0,0 +1,23 @@
+#Steph Baker
+#Enumerables
+
+class WordSplitter
+
+    include Enumerable
+
+    attr_accessor :string
+
+    def each
+        string.split(" ").each do |word|
+            yield word
+        end
+    end
+
+end
+
+splitter = WordSplitter.new
+splitter.string = "how do you do"
+
+p splitter.find_all { |word| word.include?("d") }
+p splitter.reject { |word| word.include?("d") }
+p splitter.map { |word| word.reverse }
